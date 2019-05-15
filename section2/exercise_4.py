e171ec03427840c64dd0ac22aed0433d0c129a80
Wojciech Niekrasz <wojciech.niekrasz@gmail.com>
2019-05-15 15:21:33 +0200

    finished exercise 4

--- section2/exercise_4.py
+++ section2/exercise_4.py
@@ -1,4 +1,12 @@
 # Zadanie 4
-# Napisz skrypt, który zapyta użytkownika o trzy liczby całkowite,
-# a następnie pomnóż dwa pierwsze. przed podzieleniem wyniku przez trzecią liczbę. 
-# Wejście i wyjście powinny być zrozumiałe dla użytkownika.
+# Napisz skrypt, który zapyta uzytkownika o trzy liczby calkowite,
+# a nastepnie pomnoz dwa pierwsze. przed podzieleniem wyniku przez trzecia liczbe. 
+# Wejscie i wyjscie powinny byc zrozumiale dla uzytkownika.
+
+num1 = int(input("Podaj pierwsza liczbe: "))
+num2 = int(input("Podaj druga liczbe: "))
+num3 = int(input("Podaj trzecia liczbe: "))
+
+result = num1 * num2 / num3
+
+print("Wynik wynosi:", result)
