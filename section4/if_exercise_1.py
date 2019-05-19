2149333d52b13a389b83faebc3726b49e83b6160
Wojciech Niekrasz <wojciech.niekrasz@gmail.com>
2019-05-20 00:17:26 +0200

    added section4

--- section4/if_exercise_1.py
+++
@@ -1,6 +0,0 @@
-num = int(input("Podaj liczbe: "))
-
-if num % 3 == 0:
-    print("Liczba jest podzielna przez 3")
-else:
-    print("Liczbe nie jest podzielna przez 3")
--- section4/loops_exercise_1.py
+++
@@ -1,6 +0,0 @@
-names = str(input("Podaj imiona po spacji: "))
-
-names_list = names.split()
-
-for i in names_list:
-    print("Hello", i)
